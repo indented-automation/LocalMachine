85670f955b70a20bce35e6eb8e3c0ea5e4495d9f
Jurjen Ladenius <jurjen.ladenius@effectory.com>
2021-08-12 12:26:56 +0200

    Resolve-Path does not have a childpath parameter, Join-Path has the desired effect.

--- MultiFile/LocalMachine/LocalMachine.psm1
+++ MultiFile/LocalMachine/LocalMachine.psm1
@@ -1,5 +1,5 @@
 'public', 'private' |
-    Resolve-Path -Path $PSScriptRoot -ChildPath { $_ } |
+    Join-Path -Path $PSScriptRoot -ChildPath { $_ } |
     Get-ChildItem -Recurse -File -Filter *.ps1 |
     ForEach-Object {
         . $_.FullName
